51d9e4d7cec5c70ee9612f5f9ec4151541367ed8
tmstani23 <tmstani23@gmail.com>
2020-09-14 15:27:10 -0500

    code analysis 01
    Working on bug with knex connecting to postgres server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const pg = require('pg');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -13,16 +15,20 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
+    user : '',
     password : '',
-    database : 'smart-brain'
-  }
+    database : 'smart-brain',
+  },
 });
+
+console.log(db.connection());
 
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
+app.set("db", db);
+
 
 app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
@@ -30,6 +36,7 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');

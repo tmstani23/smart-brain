310da6a1026d4d963de97f69d8610df97bc3d52b
tmstani23 <tmstani23@gmail.com>
2020-09-15 13:12:35 -0500

    code analysis 02
    Fixed bug with connection issue by updating server dependencies.  Added env file and reviewed code components.  App is working as expected.

--- server/server.js
+++ server/server.js
@@ -14,25 +14,25 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : '',
-    password : '',
-    database : 'smart-brain',
+    host : process.env.DB_HOST,
+    user : process.env.DB_OWNER,
+    password : process.env.DB_PASS,
+    database : process.env.DATABASE,
   },
 });
 
-console.log(db.connection());
-
 const app = express();
-
 app.use(cors())
 app.use(bodyParser.json());
-app.set("db", db);
 
-
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { 
+  res.send(db.users) 
+})
 app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { 
+  
+  register.handleRegister(req, res, db, bcrypt) 
+})
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})

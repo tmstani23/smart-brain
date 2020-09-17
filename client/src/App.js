de43ff4d64bd61cac9bf71aec74ab9a49aef5272
tmstani23 <tmstani23@gmail.com>
2020-09-17 07:28:55 -0500

    Code analysis 03
    Added functionality to detect multiple faces in smart brain app.

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ const particlesOptions = {
 const initialState = {
   input: '',
   imageUrl: '',
-  box: {},
+  boxes: [],
   route: 'signin',
   isSignedIn: false,
   user: {
@@ -53,20 +53,26 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
+    const faceDimensions = data.outputs[0].data.regions.map((face) => {
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      const clairifaiFace = face.region_info.bounding_box;
+      
+      return {
+        leftCol: clairifaiFace.left_col * width,
+        topRow: clairifaiFace.top_row * height,
+        rightCol: width - (clairifaiFace.right_col * width),
+        bottomRow: height - (clairifaiFace.bottom_row * height),
+      }
+    })
+    return [...faceDimensions]
+
   }
 
-  displayFaceBox = (box) => {
-    this.setState({box: box});
+  displayFaceBox = (boxes) => {
+    
+    this.setState({boxes: boxes});
   }
 
   onInputChange = (event) => {
@@ -99,6 +105,7 @@ class App extends Component {
             .catch(console.log)
 
         }
+        //console.log(response);
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
       .catch(err => console.log(err));
@@ -114,7 +121,7 @@ class App extends Component {
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
+    const { isSignedIn, imageUrl, route, boxes } = this.state;
     return (
       <div className="App">
          <Particles className='particles'
@@ -132,7 +139,7 @@ class App extends Component {
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
               />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
+              <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
             </div>
           : (
              route === 'signin'

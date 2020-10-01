e878bc85a2aa2f1d18c04350d48f8aa59990ce12
tmstani23 <tmstani23@gmail.com>
2020-10-01 10:40:02 -0500

    jwt 03
    Added profile form component.  Added server route to handle updating user in db.  Added front end fetch call logic to the profile component.  Updated displays and styles for the profile component.  The component now displays the profile information form and updates the name with the form input on submit.

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,9 @@ const initialState = {
     name: '',
     email: '',
     entries: 0,
-    joined: ''
+    joined: '',
+    pet: '',
+    age: '',
   }
 }
 
@@ -132,7 +134,7 @@ class App extends Component {
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, boxes, isProfileOpen } = this.state;
+    const { isSignedIn, imageUrl, route, boxes, isProfileOpen, user } = this.state;
     return (
       <div className="App">
         <Particles className='particles'
@@ -143,7 +145,12 @@ class App extends Component {
         />
         {isProfileOpen 
           ? <Modal> 
-            <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal} />
+            <Profile 
+              isProfileOpen={isProfileOpen} 
+              toggleModal={this.toggleModal}
+              user={user} 
+              loadUser={this.loadUser}
+            />
           </Modal>
           : null
         }

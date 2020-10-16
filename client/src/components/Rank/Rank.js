75cf815772ddc8617efa80ab515c89fa1d8be561
tmstani23 <tmstani23@gmail.com>
2020-10-16 13:04:59 -0500

    jwt 9
    Added emoji state.  Added function that calls the serverless api and gets the emoji.  Added emoji display to the rank component and logic to update cycle through the emoji's based on the current rank.

--- client/src/components/Rank/Rank.js
+++ client/src/components/Rank/Rank.js
@@ -1,31 +1,61 @@
 import React from 'react';
 
-const Rank = ({ name, entries, pet, age }) => {
+class Rank extends React.Component {
+  state = {
+    emoji: ''
+  }
   
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
-      </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-      {pet !== null && age !== null
-        ? [
-            <div className='white f3'>
-              {`Your current pet is ${pet}`}
-            </div>
-          , <div className='white f3'>
-              {`Your age is ${age}`}
-            </div>
-          ]
-        : null
+  generateEmoji = (entries) => {
+    //Get the emoji from aws serverless api
+    fetch(`https://mgni27510g.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+      .then(res => res.json())
+      // add emoji to component state
+      .then(data => this.setState({emoji: data.input}))
+      .catch(console.log)
+  }
 
-      }
-      
-      
-    </div>
-  );
+  componentDidMount() {
+    //Get the emoji
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    //Update the emoji if the image count is updated
+    if(prevProps !== this.props) {
+      return null
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  render () {
+    const { name, entries, pet, age } = this.props;
+    
+    return (
+      <div>
+        <div className='white f3'>
+          {`${name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>
+          {entries}
+        </div>
+        <div className='white f3'>
+          {`Rank Emoji: ${this.state.emoji}`}
+        </div>
+        {pet !== null && age !== null
+          ? [
+              <div className='white f3'>
+                {`Your current pet is ${pet}`}
+              </div>
+            , <div className='white f3'>
+                {`Your age is ${age}`}
+              </div>
+            ]
+          : null
+
+        }
+      </div>
+    );
+  }
 }
 
 export default Rank;
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -36,7 +36,7 @@ class Register extends React.Component {
       .then(user => {
         if (user.id) {
           this.props.loadUser(user)
-          this.props.onRouteChange('home');
+          this.props.onRouteChange('signin');
         }
       })
   }
